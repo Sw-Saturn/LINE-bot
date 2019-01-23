476e92030b59a8ceeeccaf70b3611cf5abfe41f4
Sw_Saturn <demizu.1203@gmail.com>
2019-01-24 05:08:03 +0900

    [ADD] chat

---
+++ chat.py
@@ -0,0 +1,55 @@
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,TemplateSendMessage,ButtonsTemplate,MessageAction
+)
+import settings
+import json
+import requests
+from datetime import date
+from pprint import *
+import pymysql.cursors
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi(settings.access_token)
+handler = WebhookHandler(settings.access_secret)
+
+@app.route("/")
+def hello_world():
+    return "hello world"
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    # userId を取得 (1)
+    body_json = json.loads(body)
+    app.logger.info('User Id: {}'.format(body_json['events'][0]['source']['userId']))
+    # handle webhook body
+    userAdd(body_json)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",debug=True)
