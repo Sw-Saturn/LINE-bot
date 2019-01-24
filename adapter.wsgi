8446b11b15fc07d875f9045f59dcd7da292c4f10
Kanta Demizu <demizu.1203@gmail.com>
2019-01-24 18:30:12 +0900

    [ADD] wsgi

---
+++ adapter.wsgi
@@ -0,0 +1,5 @@
+# coding: utf-8
+import sys
+sys.path.insert(0, '/srv/http/LINE-bot')
+
+from chat import app as application
--- chat.py
+++ chat.py
@@ -59,5 +59,4 @@ def handle_message(event):
         TextSendMessage(text=word))
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0",port=port)
+    app.run(host='0.0.0.0')
